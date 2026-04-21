bddd7f52369007a68129e458bc602118b1a3b601
Patryk Posluszny <patryk.posluszny27@gmail.com>
2026-04-21 14:22:23 +0200

    Lage NAV

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Nav from './components/nav'
+import { Routes, Route } from "react-router-dom";
+
 import './App.css'
 
 function App() {
@@ -9,7 +12,12 @@ function App() {
 
   return (
     <>
+      <Nav />
 
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/:slug" element={<h1>Member page</h1>} />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/components/nav.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import client from '../helpers/client'
+
+  export default function Nav(){
+    const [medlemer, setMedlemer] = useState([])
+
+    useEffect(() => {
+        async function fetchAllMedlemer() {
+          const allMedlemer = await client.fetch("*[_type == 'medlemer']{fulname, slug}")
+          setMedlemer(allMedlemer)
+        }
+    
+        fetchAllMedlemer()
+      }, [])
+
+    console.log(medlemer)
+
+    return(
+      
+                <nav> 
+                    {medlemer.map((c, index) => (<Link key={index} to={"/" + c.slug.current}> {c.fulname}</Link>))}
+                </nav>
+
+    )
+  }
